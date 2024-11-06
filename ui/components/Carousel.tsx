dce09372f5e0b4f38a7623dc82b0dd38f1f49935
p34-pac <peterpaultolulope@gmail.com>
2024-11-07 00:08:09 +0100

    layout fix carousel

--- ui/components/Carousel.tsx
+++ ui/components/Carousel.tsx
@@ -5,7 +5,6 @@ import { Button, Carousel as CarouselFlow } from "flowbite-react";
 import clsx from "clsx";
 import { glassMorphism } from "../styles/style";
 import { useRouter } from "next/navigation";
-
 import {
   CaretLeft,
   CaretRight,
@@ -19,25 +18,19 @@ type Props = {
   animeTrend?: anilistTrending[];
   loading?: boolean;
 };
-
-type CarouselItemProps = {
+type CarouselItemProp = {
   imgUrl?: string;
   title?: string;
   rate?: number;
   id?: string | number;
 };
 
-const CarouselItem: React.FC<CarouselItemProps> = ({
-  imgUrl,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  id,
-  rate,
-  title,
-}) => {
+function CarouselItem({ imgUrl, id, rate, title }: CarouselItemProp) {
   const router = useRouter();
-
-  const handleOpen = () => router.push(`/anime?anime_id=${title}`);
-
+  const navigate = router.push;
+  function handleOpen() {
+    navigate(`/anime?anime_id=${id}`);
+  }
   return (
     <div className="flex size-full items-end justify-end relative">
       <div className="size-full z-0 bg-gray-500 absolute top-0 left-0 flex items-center justify-center">
@@ -45,10 +38,7 @@ const CarouselItem: React.FC<CarouselItemProps> = ({
           <img
             className="size-full object-center object-cover"
             src={imgUrl}
-            alt={`${title} image`}
-            width={500}
-            height={500}
-            loading="lazy"
+            alt={title + " image"}
           />
         ) : (
           <ImageIcon className="text-3xl animate-pulse" />
@@ -61,7 +51,7 @@ const CarouselItem: React.FC<CarouselItemProps> = ({
           </li>
         )}
         {title && <li className="font-bold">{title}</li>}
-        <li className="flex items-center gap-2">
+        <li className="flex items-center justify-start gap-2">
           <Link href={`/watch?anime_episode=${title}-episode-1`}>
             <Button onClick={handleOpen} className="!bg-primary *:p-1">
               Watch now
@@ -76,29 +66,52 @@ const CarouselItem: React.FC<CarouselItemProps> = ({
       </ul>
     </div>
   );
-};
-
-const Carousel: React.FC<Props> = ({ animeTrend, loading }) => {
-  const renderCarouselItem = (anime: anilistTrending, index: number) => (
-    <CarouselItem
-      key={index}
-      imgUrl={anime.coverImage.extraLarge}
-      id={anime.id}
-      rate={anime.averageScore}
-      title={anime.title.userPreferred}
-    />
-  );
-
+}
+function Carousel({ animeTrend, loading }: Props) {
   return (
     <CarouselFlow
       slide={false}
       className={clsx("w-full h-72 sm:h-96 isolate", glassMorphism.className)}
       indicators={false}
-      leftControl={renderControlButton(CaretLeft, "left")}
-      rightControl={renderControlButton(CaretRight, "right")}
+      leftControl={
+        <span
+          className={clsx(
+            "h-1/4 flex items-center justify-center absolute left-0 top-1/2 -translate-y-1/2",
+            "bg-black/30 rounded-r-md",
+            "px-1 min-[300px]:px-2 sm:px-4"
+          )}
+        >
+          <span
+            className={clsx(
+              "flex items-center justify-center p-2 text-lg min-[300px]:text-2xl sm:text-3xl rounded-full",
+              glassMorphism.className
+            )}
+          >
+            <CaretLeft />
+          </span>
+        </span>
+      }
+      rightControl={
+        <span
+          className={clsx(
+            "h-1/4 flex items-center justify-center absolute right-0 top-1/2 -translate-y-1/2",
+            "bg-black/30 rounded-l-md",
+            "px-1 min-[300px]:px-2 sm:px-4"
+          )}
+        >
+          <span
+            className={clsx(
+              "flex items-center justify-center p-2 text-lg min-[300px]:text-2xl sm:text-3xl rounded-full",
+              glassMorphism.className
+            )}
+          >
+            <CaretRight />
+          </span>
+        </span>
+      }
     >
       {loading ? (
-        <div className="flex size-full items-center justify-center relative">
+        <div className="flex size-full  items-center justify-center relative">
           <CircleNotch
             weight="bold"
             size={25}
@@ -106,33 +119,19 @@ const Carousel: React.FC<Props> = ({ animeTrend, loading }) => {
           />
         </div>
       ) : (
-        animeTrend?.map(renderCarouselItem)
+        animeTrend &&
+        animeTrend.map((anime, index) => (
+          <CarouselItem
+            key={index}
+            imgUrl={anime.coverImage.extraLarge}
+            id={anime.id}
+            rate={anime.averageScore}
+            title={anime.title.userPreferred}
+          />
+        ))
       )}
     </CarouselFlow>
   );
-};
-
-// Control Button for Carousel
-const renderControlButton = (
-  Icon: React.ComponentType,
-  position: "left" | "right"
-) => (
-  <span
-    className={clsx(
-      "h-1/4 flex items-center justify-center absolute top-1/2 -translate-y-1/2",
-      `bg-black/30 rounded-${position === "left" ? "r" : "l"}-md`,
-      "px-1 min-[300px]:px-2 sm:px-4"
-    )}
-  >
-    <span
-      className={clsx(
-        "flex items-center justify-center p-2 text-lg min-[300px]:text-2xl sm:text-3xl rounded-full",
-        glassMorphism.className
-      )}
-    >
-      <Icon />
-    </span>
-  </span>
-);
+}
 
 export default Carousel;
