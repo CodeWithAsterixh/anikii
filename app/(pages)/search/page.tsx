60907402956f1345a46b7d587b394936c0217981
p34-pac <peterpaultolulope@gmail.com>
2025-02-06 21:23:08 +0100

    search update

--- app/(pages)/search/page.tsx
+++ app/(pages)/search/page.tsx
@@ -1,51 +1,21 @@
 "use client";
-import useQuery from "@/hooks/useQuery";
-import {
-    groupByTitle,
-    GroupedResult,
-} from "@/lib/mods/functions/groupAnimeArrayByTitle";
-import { AnimeListItem } from "@/lib/types/anime/__animeListItem";
 import Loader from "@/ui/components/Loader/Loader";
-import SearchResult from "@/ui/sections/searchResult/searchResult";
-import { useSearchParams } from "next/navigation";
-import { Suspense, useEffect, useState } from "react";
+import MainSearchResult from "@/ui/sections/searchResult/MainSearchResult";
+import { Suspense } from "react";
 
-export default function SearchPae() {
-  const params = useSearchParams();
-  const keyWord = params.get("for");
-
-  const searchResult = useQuery<AnimeListItem[]>(`/search?for=${keyWord}`);
-  const [data, setData] = useState<GroupedResult>();
-
-  useEffect(() => {
-    if (searchResult.data) {
-      const newData = groupByTitle(searchResult.data);
-      setData(newData);
-    }
-  }, [searchResult.data]);
-
+export default function SearchPage() {
   const loader = (
     <div className="w-full h-[50vh] flex flex-col items-center justify-center gap-4">
       <Loader />
       <strong className="animate-pulse text-tertiary opacity-50">
-        Loading {keyWord}...
+        Loading ...
       </strong>
     </div>
   );
 
   return (
-    <div className="w-full text-tertiary flex flex-col gap-3 pt-2">
-      <Suspense fallback={loader}>
-        {data ? (
-          <SearchResult
-            data={data}
-            searchedFor={keyWord ?? "undefined"}
-            totalLength={searchResult.data?.length}
-          />
-        ) : (
-          loader
-        )}
-      </Suspense>
-    </div>
+    <Suspense fallback={loader}>
+      <MainSearchResult />
+    </Suspense>
   );
 }
---
+++ ui/sections/searchResult/MainSearchResult.tsx
@@ -0,0 +1,50 @@
+"use client";
+import useQuery from "@/hooks/useQuery";
+import {
+    groupByTitle,
+    GroupedResult,
+} from "@/lib/mods/functions/groupAnimeArrayByTitle";
+import { AnimeListItem } from "@/lib/types/anime/__animeListItem";
+import Loader from "@/ui/components/Loader/Loader";
+import SearchResult from "@/ui/sections/searchResult/searchResult";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function MainSearchResult() {
+  const params = useSearchParams();
+  const keyWord = params.get("for");
+
+  const searchResult = useQuery<AnimeListItem[]>(`/search?for=${keyWord}`);
+  const [data, setData] = useState<GroupedResult>();
+
+  useEffect(() => {
+    if (searchResult.data) {
+      const newData = groupByTitle(searchResult.data);
+      setData(newData);
+    }
+  }, [searchResult.data]);
+
+  const loader = (
+    <div className="w-full h-[50vh] flex flex-col items-center justify-center gap-4">
+      <Loader />
+      <strong className="animate-pulse text-tertiary opacity-50">
+        Loading {keyWord}...
+      </strong>
+    </div>
+  );
+
+  return (
+    <div className="w-full text-tertiary flex flex-col gap-3 pt-2">
+      
+      {data ? (
+          <SearchResult
+            data={data}
+            searchedFor={keyWord ?? "undefined"}
+            totalLength={searchResult.data?.length}
+          />
+        ) : (
+          loader
+        )}
+    </div>
+  );
+}
