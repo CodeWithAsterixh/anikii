4bb1d5402554974e490b1ff81d28af1ac9b5e670
CodeWithAsterixh <peterpaultolulope@gmail.com>
2026-01-04 17:35:24 +0100

    feat(constants): add baseurl, seasons and genres constants
    Add new constant files for base URL configuration, anime seasons data, and anime genres with associated colors. These constants will be used across the application for consistent configuration and theming.

---
+++ lib/constants/baseurl.ts
@@ -0,0 +1,1 @@
+export const __BASEURL__ = import.meta.env.VITE__BASEURL__;
---
+++ lib/constants/genres.ts
@@ -0,0 +1,44 @@
+export const genres = [
+              "Action",
+              "Adventure",
+              "Comedy",
+              "Drama",
+              "Ecchi",
+              "Fantasy",
+              "Hentai",
+              "Horror",
+              "Mahou Shoujo",
+              "Mecha",
+              "Music",
+              "Mystery",
+              "Psychological",
+              "Romance",
+              "Sci-Fi",
+              "Slice of Life",
+              "Sports",
+              "Supernatural",
+              "Thriller",
+            ]
+
+            export const genreColors = [
+                "#FF4500", // Action - Bold and fiery red
+                "#FF8C00", // Adventure - Vibrant orange
+                "#FFD700", // Comedy - Cheerful yellow
+                "#8B4513", // Drama - Earthy brown
+                "#FF69B4", // Ecchi - Playful pink
+                "#7FFF00", // Fantasy - Enchanting green
+                "#9400D3", // Deep purple
+                "#000000", // Horror - Dark black
+                "#DA70D6", // Mahou Shoujo - Magical lavender
+                "#4682B4", // Mecha - Metallic blue
+                "#1E90FF", // Music - Dynamic blue
+                "#696969", // Mystery - Shadowy gray
+                "#2F4F4F", // Psychological - Deep teal
+                "#FFB6C1", // Romance - Soft pink
+                "#00CED1", // Sci-Fi - Futuristic teal
+                "#F5DEB3", // Slice of Life - Warm beige
+                "#32CD32", // Sports - Energetic lime green
+                "#8A2BE2", // Supernatural - Ethereal violet
+                "#DC143C", // Thriller - Intense crimson
+              ];
+              
