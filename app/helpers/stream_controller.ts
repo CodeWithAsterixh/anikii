d2fbe37c53aa36d437ad3f295ad75fcdc920c1be
CodeWithAsterixh <peterpaultolulope@gmail.com>
2026-01-07 23:20:51 +0100

    refactor(stream): remove unused stream url generation functions
    The functions for generating proxied, direct download and live stream URLs were removed as they were no longer being used in the application. The corresponding hook `use_stream` was also deleted since its functionality was no longer needed.

--- app/helpers/stream_controller.ts
+++ app/helpers/stream_controller.ts
@@ -1,5 +1,5 @@
 import { api_client } from "./api_client";
-import type { IApiSuccessEnvelope, IStreamMetadata, IEpisodeExtra, StreamType } from "../types";
+import type { IApiSuccessEnvelope, IStreamMetadata, IEpisodeExtra } from "../types";
 import { StreamMetadataEnvelopeSchema, EpisodeExtraEnvelopeSchema } from "./schemas";
 
 export const get_stream_metadata = async (id: number) => {
@@ -30,20 +30,4 @@ export const get_episode_extra = async (id: number, ep: number) => {
     `/anime/${id}/stream/ep/${ep}/extra`
   );
   return EpisodeExtraEnvelopeSchema.parse(response);
-};
-
-export const get_proxied_download_url = (id: number, ep: number, type: StreamType = "sub") => {
-  return `${api_client.defaults.baseURL}/anime/${id}/stream/ep/${ep}/download?type=${type}`;
-};
-
-export const get_direct_download_url = (id: number, ep: number, type: StreamType = "sub", provider?: string) => {
-  let url = `${api_client.defaults.baseURL}/anime/${id}/stream/ep/${ep}/download-direct?type=${type}`;
-  if (provider) url += `&provider=${provider}`;
-  return url;
-};
-
-export const get_live_stream_url = (id: number, ep: number, type: StreamType = "sub", provider?: string) => {
-  let url = `${api_client.defaults.baseURL}/anime/${id}/stream/ep/${ep}/live?type=${type}`;
-  if (provider) url += `&provider=${provider}`;
-  return url;
 };
